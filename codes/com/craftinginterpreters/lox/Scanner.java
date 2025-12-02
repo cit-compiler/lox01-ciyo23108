650b3ef10ba09a47b2ec0c56732e84d1bcebbbe2
ciyo23108 <ciyo23108@g.nihon-u.ac.jp>
2025-12-02 10:08:49 +0900

    update scanToken

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -42,6 +42,26 @@ public class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break; 
+            case '!':
+              addToken(match('=') ? BANG_EQUAL : BANG);
+              break;
+            case '=':
+              addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+              break;
+            case '<':
+              addToken(match('=') ? LESS_EQUAL : LESS);
+              break;
+            case '>':
+              addToken(match('=') ? GREATER_EQUAL : GREATER);
+              break;
+            case '/':
+              if (match('/')) {
+                // A comment goes until the end of the line.
+                while (peek() != '\n' && !isAtEnd()) advance();
+              } else {
+                addToken(SLASH);
+              }
+              break;
 
             default:
                 Lox.error(line, "Unexpected character.");
@@ -66,4 +86,18 @@ public class Scanner {
     String text = source.substring(start, current);
     tokens.add(new Token(type, text, literal, line));
   }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
 }
