85f676bb3c4b98302dd70c693be43b2d23e65067
ciyo23108 <ciyo23108@g.nihon-u.ac.jp>
2025-12-02 11:50:55 +0900

    add number and identifier method

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -8,11 +8,33 @@ import java.util.Map;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 public class Scanner {
-    private final String source;
-    private final List<Token> tokens = new ArrayList<>();
-    private int start = 0;
-    private int current = 0;
-    private int line = 1;
+private final String source;
+private final List<Token> tokens = new ArrayList<>();
+private static final Map<String, TokenType> keywords;  
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
+
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
 
      Scanner(String source) {
         this.source = source;
@@ -74,12 +96,63 @@ public class Scanner {
 
             case '"': string(); break;
 
+            case 'o':
+              if (match('r')) {
+                addToken(OR);
+              }
+              break;
+
             default:
+              if (isDigit(c)) {
+                number();
+              } else if (isAlpha(c)) {
+                identifier();  
+              } else {
                 Lox.error(line, "Unexpected character.");
+              }
                 break;
     }
     }
   
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  
+    private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
+  }  
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  } 
 
      private boolean isAtEnd() {
         return current >= source.length();
@@ -111,7 +184,11 @@ public class Scanner {
     return source.charAt(current);
   }
 
-   private void string() {
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  } 
+
+  private void string() {
     while (peek() != '"' && !isAtEnd()) {
       if (peek() == '\n') line++;
       advance();
@@ -129,5 +206,5 @@ public class Scanner {
     String value = source.substring(start + 1, current - 1);
     addToken(STRING, value);
   }
-  
+
 }
