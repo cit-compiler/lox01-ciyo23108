b2db3d7b47fd940969f338c6efba25b86d38e8a3
ciyo23108 <ciyo23108@g.nihon-u.ac.jp>
2025-12-16 11:33:34 +0900

    Lox.java and Parser.java and Sample.lox

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -43,6 +43,14 @@ public class Lox {
         private static void run(String source) {
             Scanner scanner = new Scanner(source);
             List<Token> tokens = scanner.scanTokens();
+            Parser parser = new Parser(tokens);
+            Expr expression = parser.parse();
+
+            // Stop if there was a syntax error.
+            if (hadError) return;
+
+            System.out.println(new AstPrinter().print(expression));
+
 
             // For now, just print the tokens.
             for (Token token : tokens) {
@@ -66,4 +74,5 @@ public class Lox {
       report(token.line, " at '" + token.lexeme + "'", message);
     }
   }
+}
 
